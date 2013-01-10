a13290038a50af75f13cb6736cbc774ed6540c89
james2doyle <james2doyle@gmail.com>
2013-01-10 12:49:13 -0500

    revamp folder structure

--- views/display.php
+++
@@ -1,3 +0,0 @@
-<div class="pyro-image-wiget">
-<img src="{{ files:image_url id="<?php echo $options['image_id']; ?>" }}" />
-</div>
--- views/form.php
+++
@@ -1,6 +0,0 @@
-<ul>
-  <li>
-    <label>Image</label>
-    <?php echo form_dropdown('image_id', $image_select, $options['image_id']); ?>
-  </li>
-</ul>
