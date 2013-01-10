fda29b6e46c750773d84c51ddfa6159b425f22c2
james2doyle <james2doyle@gmail.com>
2013-01-10 12:55:26 -0500

    fixed submodule mess

---
+++ ImageWidget/views/display.php
@@ -0,0 +1,3 @@
+<div class="pyro-image-wiget">
+<img src="{{ files:image_url id="<?php echo $options['image_id']; ?>" }}" />
+</div>
---
+++ ImageWidget/views/form.php
@@ -0,0 +1,6 @@
+<ul>
+  <li>
+    <label>Image</label>
+    <?php echo form_dropdown('image_id', $image_select, $options['image_id']); ?>
+  </li>
+</ul>
